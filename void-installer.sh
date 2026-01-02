755a9a037d2b7c40638e263d696d976fe4d0b657
konfuzhon <b8s13.94i@gmail.com>
2026-01-02 14:00:53 -0500

    Added btop and fetch alias to installer

--- void-installer.sh
+++ void-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo xbps-install -Sy base-devel xorg xinit libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel
-sudo xbps-install -y neovim dash imlib2 imlib2-devel xsetroot nerd-fonts picom feh kitty acpi rofi htop fastfetch
+sudo xbps-install -y neovim dash imlib2 imlib2-devel xsetroot nerd-fonts picom feh kitty acpi rofi btop htop fastfetch
 cd ~/.config/chadwm
 mv eww ..
 mv rofi ..
@@ -11,5 +11,6 @@ mv btop ..
 cd chadwm
 sudo make install
 echo "alias chadwm='startx ~/.config/chadwm/scripts/run.sh'" >> ~/.bashrc
+echo "alias fetch='/home/$USER/.config/chadwm/scripts/fetch'" >> ~/.bashrc
 echo "Installation complete!"
 echo "Please reboot your computer."
